a9778c145afc2e39f6a47b75bed103606cd64081
Masashi Miyaura <masasam@users.noreply.github.com>
2017-10-08 19:29:40 +0900

    Changed to display if amazon-s3-deploy error

--- easy-jekyll.el
+++ easy-jekyll.el
@@ -887,7 +887,12 @@ POST-FILE needs to have and extension '.md' or '.textile'."
      (error "Please set 'easy-jekyll-amazon-s3-bucket-name' variable"))
    (when (file-directory-p "_site")
      (delete-directory "_site" t nil))
-   (shell-command-to-string "bundle exec jekyll build --destination _site")
+   (let ((ret (call-process "bundle" nil "*jekyll-amazon-s3-deploy*" t "exec" "jekyll" "build" "--destination" "_site")))
+     (unless (zerop ret)
+       (switch-to-buffer (get-buffer "*jekyll-amazon-s3-deploy*"))
+       (error "'bundle exec jekyll build' command does not end normally")))
+   (when (get-buffer "*jekyll-amazon-s3-deploy*")
+     (kill-buffer "*jekyll-amazon-s3-deploy*"))
    (shell-command-to-string (concat "aws s3 sync --delete _site s3://" easy-jekyll-amazon-s3-bucket-name "/"))
    (message "Blog deployed")
    (when easy-jekyll-url
